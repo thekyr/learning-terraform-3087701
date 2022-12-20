426588f732fc4a2612169bd8fb0666f156c34285
Theodor Kyriakidis <thekyr@users.noreply.github.com>
2022-12-20 16:31:25 +0200

    fix sg

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "blog-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
@@ -89,11 +89,12 @@ module "alb" {
 }
     
 module "blog_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
   name        = "blog"
   description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = module.blog_vpc.vpc_id
+  vpc_id      = module.vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
