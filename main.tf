d065df0c24978a2e443dfbbe5f8d110365b163e2
Theodor Kyriakidis <thekyr@users.noreply.github.com>
2022-12-20 15:55:17 +0200

    update dev vpc

--- main.tf
+++ main.tf
@@ -51,8 +51,8 @@ module "blog_sg" {
   vpc_id = module.vpc.public_subnets[0]
     
   ingress_rules = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks  = ["62.103.176.173/32"]
+  ingress_cidr_blocks  = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]
-  egress_cidr_blocks  = ["62.103.176.173/32"]
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
