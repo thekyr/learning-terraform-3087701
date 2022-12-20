e692fb80453d34b46bdced3d927d27999f515e22
Theodor Kyriakidis <thekyr@users.noreply.github.com>
2022-12-20 16:11:36 +0200

    update vpc

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -48,7 +49,7 @@ module "blog_sg" {
 
   name        = "blog"
   description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = module.vpc.public_subnetsall
+  vpc_id      = module.blog_vpc.vpc_id
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
