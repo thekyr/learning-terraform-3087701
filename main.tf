1858bf9469d360ba487455c6b7c1c6738e208b86
Theodor Kyriakidis <thekyr@users.noreply.github.com>
2022-12-20 16:59:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,64 +44,10 @@ resource "aws_instance" "blog" {
   }
 }
 
-<<<<<<< HEAD
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
-
-  name = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
-
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = "dev"
-  }
-}
-    
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
-
-  name        = "blog"
-  description = "Security group for web-server with HTTP ports open within VPC"
-  vpc_id      = module.vpc.public_subnets[0]
-=======
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
   name    = "blog_new"
->>>>>>> parent of 1f308c4 (remove_deprecated_code)
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
@@ -146,4 +92,3 @@ resource "aws_security_group_rule" "blog_everything_out" {
 
   security_group_id = aws_security_group.blog.id
 }
-
