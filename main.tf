4ede3b58b321e77a6b855234eee6dfe7d8d5e1e5
Theodor Kyriakidis <thekyr@users.noreply.github.com>
2022-12-20 16:22:07 +0200

    syntax fix

--- main.tf
+++ main.tf
@@ -52,9 +52,9 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  vpc_id             = [module.blog_vpc.vpc_id]
+  subnets            = [module.blog_vpc.public_subnets]
+  security_groups    = [module.blog_sg.security_group_id]
 
   access_logs = {
     bucket = "my-alb-logs"
@@ -87,66 +87,6 @@ module "alb" {
     Environment = "dev"
   }
 }
-HTTP to HTTPS redirect and HTTPS cognito authentication:
-
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
-
-  name = "my-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = "vpc-abcde012"
-  subnets            = ["subnet-abcde012", "subnet-bcde012a"]
-  security_groups    = ["sg-edcd9784", "sg-edcd9785"]
-
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
-  target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "HTTPS"
-      backend_port     = 443
-      target_type      = "instance"
-    }
-  ]
-
-  https_listeners = [
-    {
-      port                 = 443
-      protocol             = "HTTPS"
-      certificate_arn      = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-      action_type          = "authenticate-cognito"
-      target_group_index   = 0
-      authenticate_cognito = {
-        user_pool_arn       = "arn:aws:cognito-idp::123456789012:userpool/test-pool"
-        user_pool_client_id = "6oRmFiS0JHk="
-        user_pool_domain    = "test-domain-com"
-      }
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port        = 80
-      protocol    = "HTTP"
-      action_type = "redirect"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-  ]
-
-  tags = {
-    Environment = "Test"
-  }
-}
-    
     
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
