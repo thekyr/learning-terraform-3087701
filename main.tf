46afe5024783af3fc090ebf0e48d2aacfb0fc470
Theodor Kyriakidis <thekyr@users.noreply.github.com>
2022-12-20 16:03:24 +0200

    update vpc

--- main.tf
+++ main.tf
@@ -45,11 +45,11 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
-  name    = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
-    
+  name        = "blog"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = module.vpc.public_subnets[0]
+
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
 
